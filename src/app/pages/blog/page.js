a103402dc4dcc7ad7eab00591abfa4d7060025f8
ibr5500 <ibr.s.ahmat@gmail.com>
2024-10-08 20:40:09 +0300

    Add blog header section for the Desktop version

--- src/app/pages/blog/page.js
+++ src/app/pages/blog/page.js
@@ -1,10 +1,5 @@
-import React from "react";
-import {
-  IoIosArrowBack,
-  IoIosArrowForward,
-  IoIosArrowDown,
-  IoIosArrowUp,
-} from "react-icons/io";
+import React from 'react';
+import { IoIosArrowBack, IoIosArrowForward, IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 import {
   FaRegClock,
   FaRegCommentAlt,
@@ -12,12 +7,8 @@ import {
   FaSearch,
   FaFacebookF,
   FaLinkedinIn,
-} from "react-icons/fa";
-import { FaRegEye } from "react-icons/fa6";
-
-
-
-
+} from 'react-icons/fa';
+import { FaRegEye } from 'react-icons/fa6';
 
 //icons
 {
@@ -37,21 +28,30 @@ import { FaRegEye } from "react-icons/fa6";
   */
 }
 
-
 //blogs api endpoint
-const url = "./api/blogs"
-
-
+const url = './api/blogs';
 
 const page = () => {
   return (
     <div className="blog-page  p-5 md:px-[140px] md:py-10 bg-background ">
-      <section className=" header-section mt-24">blog page</section>
+      <section className="header-section text-center mt-24 mx-auto w-[650px]">
+        <h2 className="text-[#0EC2F56] font-bold text-2xl my-1">
+          Our
+          <span className="text-primary"> Blogs</span>
+        </h2>
+        <p>
+          Dive into the latest trends, expert insights, and industry news that are shaping the
+          digital landscape. From cutting-edge web development to the future of mobile applications,
+          we provide thought-provoking content to help businesses stay ahead.
+        </p>
+        <br />
+        <p>Stay informed, stay inspired.</p>
+      </section>
       <section className=" recent-posts-slide "></section>
       <section className=" popular-posts "></section>
       <section className=" main-page  ">
-        <main className=" main-posts " ></main>
-        <aside className=" right-side-panel " ></aside>
+        <main className=" main-posts "></main>
+        <aside className=" right-side-panel "></aside>
       </section>
     </div>
   );
